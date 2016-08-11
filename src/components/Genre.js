3eeb34ab5210c265847e8bc9f1601fdfc3c36c28
Роман Кислов <roman@MacBook-Roman.local>
2016-08-11 17:30:20 +0500

    Dynamic rout

--- src/components/Genre.js
+++ src/components/Genre.js
@@ -2,10 +2,22 @@ import React, {Component} from 'react'
 
 export default class Genre extends Component{
     render(){
-        return(
+        let template;
+
+        if (this.props.params.release){
+            template = (
             <div className='row'>
-                <div className='col-md-12'>Раздел /genre</div>
+            <h3 className='col-md-12'>{this.props.params.genre}</h3>
+            <div className='col-md-12'>{this.props.children}</div>
             </div>
-        )
+            )
+        } else{
+        template = (
+            <div className='row'>
+            <h3 className='col-md-12'>{this.props.params.genre}</h3>
+            <div className='col-md-12'>Здесь будет список релизов</div>
+            </div>
+        )}
+        return template;
     }
 }
