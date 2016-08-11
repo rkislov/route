727d19cae95766c834a890d3fdb93081da30bc7e
Роман Кислов <roman@MacBook-Roman.local>
2016-08-11 18:09:51 +0500

    Refactoring code

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
-import {Link} from 'react-router'
+import NavLink from '../../components/NavLink'
+
+
 
 export default class App extends Component {
   
@@ -7,8 +9,8 @@ export default class App extends Component {
     return (
     <div className='container'>
     <ul className='nav nav-pills'>
-      <li><Link to='/admin'>Админка</Link></li>
-      <li><Link to='/list'>Список жанров</Link></li>
+      <li><NavLink to='/admin'>Админка</NavLink></li>
+      <li><NavLink to='/list'>Список жанров</NavLink></li>
     </ul>
     {this.props.children}
     </div>)
